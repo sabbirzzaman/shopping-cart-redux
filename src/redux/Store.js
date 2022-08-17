e9f461a4ba3f9124267542c75d86f8ad6abbc5ec
sabbirzzaman <sztonmoy7@gmail.com>
2022-08-17 23:31:27 +0600

    quantity handler middleware added

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,6 +1,22 @@
-import { createStore } from "redux";
-import rootReducer from "./rootReducer";
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+const quantityHandler = (store) => (next) => (action) => {
+    // get state
+    const state = store.getState();
+    const productsItems = state.products;
+
+    const updatedState = [...productsItems];
+
+    updatedState.map((item) => {
+        if (action.payload.name === item.name) {
+            item.quantity = item.quantity - 1;
+        }
+    });
+
+    return next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(quantityHandler));
 
 export default store;
